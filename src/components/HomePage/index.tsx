99dc7a2286778006d636b0079a735f3070642b25
Gavriel Frant <gavril44@gmail.com>
2022-02-20 22:41:00 +0200

    added navbar and start episode gallery

--- src/components/HomePage/index.tsx
+++ src/components/HomePage/index.tsx
@@ -5,8 +5,16 @@ type Props = {};
 
 export default function HomePage({}: Props) {
   return (
-    <div>
+    <div className="home-page w-full h-screen bg-cover bg-center bg-[url('https://images6.alphacoders.com/321/thumb-1920-321927.jpg')]">
       <Navbar />
+      <section
+        id="home-content"
+        className="m-auto border-2 w-4/5 flex-col align-middle"
+      >
+        <h1 className="text-6xl text-center font-bold mt-7">
+          The Breaking Bad Wiki
+        </h1>
+      </section>
     </div>
   );
 }
